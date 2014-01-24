7e14b2024440c7ccc90d794a20f3518d7d4ffa5a
j2blake <jeb228@cornell.edu>
2014-01-24 16:12:50 -0500

    Use HttpClient more rigorously in the CallbackController

--- testwebapp/src/edu/cornell/mannlib/orcidclient/testwebapp/CallbackController.java
+++ testwebapp/src/edu/cornell/mannlib/orcidclient/testwebapp/CallbackController.java
@@ -8,6 +8,7 @@ import static edu.cornell.mannlib.orcidclient.context.OrcidClientContext.Setting
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -20,11 +21,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.fluent.Content;
-import org.apache.http.client.fluent.Form;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.client.fluent.Response;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import edu.cornell.mannlib.orcidclient.auth.AccessToken;
 import edu.cornell.mannlib.orcidclient.auth.AccessTokenFormatException;
@@ -78,22 +88,29 @@ public class CallbackController extends HttpServlet {
 	private AccessToken getAccessTokenFromAuthCode(String authCode)
 			throws ClientProtocolException, IOException,
 			AccessTokenFormatException {
-		Request request = Request
-				.Post(occ.getAccessTokenRequestUrl())
-				.addHeader("Accept", "application/json")
-				.bodyForm(
-						Form.form()
-								.add("client_id", occ.getSetting(CLIENT_ID))
-								.add("client_secret",
-										occ.getSetting(CLIENT_SECRET))
-								.add("grant_type", "authorization_code")
-								.add("code", authCode)
-								.add("redirect_uri", occ.getCallbackUrl())
-								.build());
-		Response response = request.execute();
-		Content content = response.returnContent();
-		String string = content.asString();
-		return new AccessToken(string);
+
+		String uri = occ.getAccessTokenRequestUrl();
+		HttpPost postRequest = new PostRequestBuilder(uri)
+				.header("Accept", "application/json")
+				.add("client_id", occ.getSetting(CLIENT_ID))
+				.add("client_secret", occ.getSetting(CLIENT_SECRET))
+				.add("grant_type", "authorization_code").add("code", authCode)
+				.add("redirect_uri", occ.getCallbackUrl()).build();
+
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		try {
+			return httpClient.execute(postRequest,
+					new AccessTokenResponseHandler());
+		} catch (ClientProtocolException e) {
+			Throwable cause = e.getCause();
+			if (cause instanceof AccessTokenFormatException) {
+				throw (AccessTokenFormatException) cause;
+			} else {
+				throw e;
+			}
+		} finally {
+			httpClient.close();
+		}
 	}
 
 	private void fail(HttpServletRequest req, HttpServletResponse resp,
@@ -116,4 +133,74 @@ public class CallbackController extends HttpServlet {
 		return "Request: " + req.getRequestURL() + ", parameters=" + prettyMap;
 	}
 
+	private static class AccessTokenResponseHandler implements
+			ResponseHandler<AccessToken> {
+		private int statusCode;
+		private String reasonPhrase = "";
+		private String content = "";
+
+		@Override
+		public AccessToken handleResponse(HttpResponse response)
+				throws ClientProtocolException, IOException {
+			StatusLine statusLine = response.getStatusLine();
+			this.statusCode = statusLine.getStatusCode();
+			this.reasonPhrase = statusLine.getReasonPhrase();
+
+			if (statusCode >= 300) {
+				throw new HttpResponseException(statusLine.getStatusCode(),
+						statusLine.getReasonPhrase());
+			}
+			HttpEntity entity = response.getEntity();
+			if (entity == null) {
+				throw new ClientProtocolException(
+						"Response contains no content");
+			}
+			this.content = EntityUtils.toString(entity);
+			try {
+				return new AccessToken(this.content);
+			} catch (AccessTokenFormatException e) {
+				throw new ClientProtocolException(
+						"Failed to parse the access token", e);
+			}
+		}
+
+		public int getStatusCode() {
+			return statusCode;
+		}
+
+		public String getReasonPhrase() {
+			return reasonPhrase;
+		}
+
+		public String getContent() {
+			return content;
+		}
+
+	}
+
+	private static class PostRequestBuilder {
+		private final HttpPost post;
+		private final List<NameValuePair> parameters = new ArrayList<>();
+
+		PostRequestBuilder(String url) {
+			this.post = new HttpPost(url);
+		}
+
+		PostRequestBuilder add(String name, String value) {
+			parameters.add(new BasicNameValuePair(name, value));
+			return this;
+		}
+
+		PostRequestBuilder header(String name, String value) {
+			post.addHeader(name, value);
+			return this;
+		}
+
+		HttpPost build() {
+			UrlEncodedFormEntity entity = new UrlEncodedFormEntity(parameters,
+					Consts.UTF_8);
+			post.setEntity(entity);
+			return post;
+		}
+	}
 }
